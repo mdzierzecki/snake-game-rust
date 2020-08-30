3dd39683ea42cb9ca822f5fab704dea0ea764409
Mateusz Dzierzecki <mateusz@dzierzecki.com>
2020-08-30 12:33:22 +0200

    add snake render v2

--- src/main.rs
+++ src/main.rs
@@ -11,35 +11,22 @@ use piston::window::WindowSettings;
 use graphics::*;
 
 
-
-pub struct App {
-    gl: GlGraphics, // OpenGL drawing backend.
-    rotation: f64,  // Rotation for the square.
-    snake: Snake,
-}
-
 struct Snake {
     pos_x: i32,
-    pos_y: i32
+    pos_y: i32,
+    gl: GlGraphics,
+    rotation: f64,
 }
 
 impl Snake {
-    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs) {
+    fn render(&mut self, args: &RenderArgs) {
+
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
 
         let square = rectangle::square(self.pos_x as f64,
         self.pos_y as f64, 20_f64);
-    }
-}
 
-impl App {
-    fn render(&mut self, args: &RenderArgs) {
-
-        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
-        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
-
-        let square = rectangle::square(0.0, 0.0, 140.0);
-        let rotation = self.rotation;
         let (x, y) = (args.window_size[0] / 2.0, args.window_size[1] / 2.0);
 
         self.gl.draw(args.viewport(), |c, gl| {
@@ -49,7 +36,6 @@ impl App {
             let transform = c
                 .transform
                 .trans(x, y)
-                .rot_rad(rotation)
                 .trans(-25.0, -25.0);
 
             // Draw a box rotating around the middle of the screen.
@@ -63,38 +49,34 @@ impl App {
     }
 }
 
+
 fn main() {
     // Change this to OpenGL::V2_1 if not working.
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", [800, 800])
+    let mut window: Window = WindowSettings::new("snake game", [800, 800])
         .graphics_api(opengl)
         .exit_on_esc(true)
         .build()
         .unwrap();
 
-    let snake = Snake{
+
+    let mut snake = Snake{
         pos_x: 2,
         pos_y: 1,
-    };
-
-
-    // Create a new game and run it.
-    let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
-        snake: snake,
+        rotation: 0.0
     };
 
     let mut events = Events::new(EventSettings::new());
+
     while let Some(e) = events.next(&mut window) {
         if let Some(args) = e.render_args() {
-            app.render(&args);
+            snake.render(&args);
         }
-
         if let Some(args) = e.update_args() {
-            app.update(&args);
+            snake.update(&args);
         }
     }
 }
